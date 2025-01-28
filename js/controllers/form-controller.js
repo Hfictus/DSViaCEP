1311c6b134a5220ea4142be25d35a9d71bc8d2e2
Gerson Klauck <gk120hf@gmail.com>
2025-01-28 15:52:37 -0300

    form validation

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -59,9 +59,19 @@ async function handleInputCepChange(event) {
     
 }
 
-async function handleBtnSaveClick(event) {
+function handleBtnSaveClick(event) {
     event.preventDefault();
-    listController.addCard(state.address);
+    const errors = addressService.getErrors(state.address);
+    const keys = Object.keys(errors);
+    
+    if(keys.length > 0) {
+        keys.forEach(key => {
+            setFormError(key, errors[key]);
+        });
+    }else {
+        listController.addCard(state.address);
+        clearForm();
+    }
 }
 
 function handleInputNumberChange(event) {
@@ -91,10 +101,10 @@ function clearForm() {
     setFormError("cep", "");
     setFormError("number", "");
 
+    state.address = new Address();
+
     state.inputCep.focus();
-
 }
-
 
 function setFormError(key, value) {
     const element = document.querySelector(`[data-error="${key}"]`);
